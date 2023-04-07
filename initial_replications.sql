0b2bf3b21acaa1cd8d029880b2cce5c86403abe7
kennsippell@gmail.com <kennsippell@gmail.com>
2023-04-06 22:07:27 -0700

    Initial replications

---
+++ initial_replications.sql
@@ -0,0 +1,14 @@
+WITH dates AS (
+  SELECT generate_series(now() - '60 days'::interval, now(), '1 day'::interval) AS date
+)
+
+SELECT
+  dates.date,
+  COALESCE(
+    COUNT(*) FILTER(WHERE device_id IS NOT NULL)
+  , 0) AS count_initial_replications
+FROM dates	
+LEFT JOIN useview_telemetry_devices ON dates.date::date = period_start
+GROUP BY 1
+ORDER BY 1 ASC
+;

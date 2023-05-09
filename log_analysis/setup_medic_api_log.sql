d11eba8378183e440946c1f943f0980ef678bd7c
Kenn Sippell <kennsippell@gmail.com>
2023-05-09 00:28:58 -0700

    Update regex for lines without HTTP protocol version

--- log_analysis/setup_medic_api_log.sql
+++ log_analysis/setup_medic_api_log.sql
@@ -34,28 +34,28 @@ set
     else x.parsed[11]::real
   end,
   url_bucket = case
-		when url ~ 'medic-user-.*-meta/_local.*' then 'medic-user-.*-meta/_local.*'
-		when url ~ 'medic-user-.*-meta/_revs_diff.*' then 'medic-user-.*-meta/_revs_diff.*'
-		when url ~ 'medic-user-.*-meta/_changes.*' then 'medic-user-.*-meta/_changes.*'
-		when url ~ 'medic-user-.*-meta' then 'medic-user-.*-meta'
-		when url ~ '/medic/_local/.*' then '/medic/_local/.*'
-		when url ~ 'medic/.*/content' then 'medic/.*/content'
-		when url ~ 'medic/form%3.*' then 'medic/form%3.*'
-		when url ~ 'medic/resources/.*' then 'medic/resources/.*'
-		else substring(url FROM '^([^\?]*).*')
-	end,
+    when url ~ 'medic-user-.*-meta/_local.*' then 'medic-user-.*-meta/_local.*'
+    when url ~ 'medic-user-.*-meta/_revs_diff.*' then 'medic-user-.*-meta/_revs_diff.*'
+    when url ~ 'medic-user-.*-meta/_changes.*' then 'medic-user-.*-meta/_changes.*'
+    when url ~ 'medic-user-.*-meta' then 'medic-user-.*-meta'
+    when url ~ '/medic/_local/.*' then '/medic/_local/.*'
+    when url ~ 'medic/.*/content' then 'medic/.*/content'
+    when url ~ 'medic/form%3.*' then 'medic/form%3.*'
+    when url ~ 'medic/resources/.*' then 'medic/resources/.*'
+    else substring(url FROM '^([^\?]*).*')
+  end,
   express_queue = x.express_queue
 from (
-	select
-	  id,
-	  regexp_matches(raw, '\[([^\]]*)\] ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*)(?: ([^ ]*) ([^ ]*) ([^ ]*) ms)?$') as parsed,
-	  sum(
+  select
+    id,
+    regexp_matches(raw, '\[([^\]]*)\] ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*)( [^ ]*)?(?: ([^ ]*) ([^ ]*) ([^ ]*) ms)?$') as parsed,
+    sum(
       case
-	      when exchange = 'REQ' then 1
-	      else -1
-	    end
+        when exchange = 'REQ' then 1
+        else -1
+      end
     ) over (order by id asc rows between unbounded preceding and current row) as express_queue
-	from medic_api_log
+  from medic_api_log
 ) x
 where medic_api_log.id = x.id
 ;
